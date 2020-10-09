d9dc16f25aa46901b7efba77f3bd03539787d4d1
NestorMonroy <nestor.monroy.90@gmail.com>
2020-10-09 08:49:20 -0500

    adding function rate_movies

---
+++ movierater/api/serializers.py
@@ -0,0 +1,17 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Movie, Rating
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'description', 'no_of_ratings', 'avg_rating')
+
+
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('stars', 'movie', 'user')
